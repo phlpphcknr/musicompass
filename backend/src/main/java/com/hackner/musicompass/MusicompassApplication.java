9421532d470b98ae4bf0f0c35b371ba415a80d1d
PhilippHackner <philipp.hackner@protonmail.com>
2021-03-15 15:15:05 +0100

    Add backend

---
+++ backend/src/main/java/com/hackner/musicompass/MusicompassApplication.java
@@ -0,0 +1,13 @@
+package com.hackner.musicompass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicompassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicompassApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/hackner/musicompass/MusicompassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackner.musicompass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicompassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
