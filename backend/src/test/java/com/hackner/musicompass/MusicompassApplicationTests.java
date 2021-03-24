764050fc213e3dd2b67d7a2663cd904574d5f01c
PhilippHackner <philipp.hackner@protonmail.com>
2021-03-24 15:48:31 +0100

    Change discogs secret from value to configurationproperties annotation

--- backend/src/test/java/com/hackner/musicompass/MusicompassApplicationTests.java
+++ backend/src/test/java/com/hackner/musicompass/MusicompassApplicationTests.java
@@ -2,10 +2,8 @@ package com.hackner.musicompass;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@TestPropertySource(properties = "discogs.token=testtest")
 class MusicompassApplicationTests {
 
     @Test
