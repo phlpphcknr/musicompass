54acea451aff182c3ff37613f6c5f15c23e5cd5f
PhilippHackner <philipp.hackner@protonmail.com>
2021-03-30 09:13:51 +0200

    Change equals & hash code in artist class

--- backend/src/main/java/com/hackner/musicompass/model/Artist.java
+++ backend/src/main/java/com/hackner/musicompass/model/Artist.java
@@ -1,9 +1,6 @@
 package com.hackner.musicompass.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.data.annotation.Id;
 
 import java.util.Date;
@@ -19,22 +16,11 @@ public class Artist {
 
     @Id
     private String artistName;
+    @EqualsAndHashCode.Exclude
     private Date saveDate;
     private ArtistInfo artistInfo;
     private List<ArtistRelease> artistAlbums;
     private List<ArtistRelease> artistSingles;
     private RecommendationTags recommendationTags;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Artist artist = (Artist) o;
-        return Objects.equals(artistName, artist.artistName) && Objects.equals(artistInfo, artist.artistInfo) && Objects.equals(artistAlbums, artist.artistAlbums) && Objects.equals(artistSingles, artist.artistSingles) && Objects.equals(recommendationTags, artist.recommendationTags);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(artistName, artistInfo, artistAlbums, artistSingles, recommendationTags);
-    }
 }
