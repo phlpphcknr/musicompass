79eb1cb1967b5654e6ab52b273c07aff1f9a624c
PhilippHackner <philipp.hackner@protonmail.com>
2021-03-31 22:16:23 +0200

    Recommendation controller test wip

--- backend/src/test/java/com/hackner/musicompass/controller/RecommendationControllerTest.java
+++ backend/src/test/java/com/hackner/musicompass/controller/RecommendationControllerTest.java
@@ -1,8 +1,11 @@
 package com.hackner.musicompass.controller;
 
+import com.hackner.musicompass.controller.model.RecommendationTagsDto;
 import com.hackner.musicompass.db.ArtistMongoDb;
 import com.hackner.musicompass.db.RecommendationCategoryMongoDb;
+import com.hackner.musicompass.model.Artist;
 import com.hackner.musicompass.model.RecommendationCategory;
+import com.hackner.musicompass.model.RecommendationTags;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,6 +14,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 
@@ -67,15 +71,26 @@ class RecommendationControllerTest {
     @DisplayName("Change recommendation tags for already existing recommendation tags")
     public void changeExistingRecommendationTags(){
         //GIVEN
+        String artistName ="artistName";
+        List<String> genres = Arrays.asList("Genre1","Genre2");
+        List<String> roles = Arrays.asList("Role1","Role2");
+        RecommendationTagsDto recommendationTagsDto = RecommendationTagsDto.builder()
+                .artistName(artistName)
+                .genres(genres)
+                .roles(roles)
+                .gender("Gender").build();
+        artistMongoDb.save(Artist.builder().artistName(artistName).build());
 
         //WHEN
-        ResponseEntity <List<RecommendationCategory>> response = testRestTemplate.exchange(getUrl() + artistName,
+        HttpEntity<RecommendationTagsDto> entity = new HttpEntity<>(recommendationTagsDto);
+        ResponseEntity <RecommendationTags> response = testRestTemplate.exchange(getUrl() + artistName,
                 HttpMethod.POST,
-                null,
-                new ParameterizedTypeReference<List<RecommendationCategory>>() {});
-
+                entity,
+                RecommendationTags.class);
 
         //THEN
+        assertThat(response.getBody(), equalTo());
+
     }
 
 
