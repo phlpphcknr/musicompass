dbc13aad5b3c0953c185f8cfd80133f4d459478c
PhilippHackner <philipp.hackner@protonmail.com>
2021-04-03 20:55:31 +0200

    Adjust test to new rating calculation

--- backend/src/test/java/com/hackner/musicompass/service/ArtistReleaseServiceTest.java
+++ backend/src/test/java/com/hackner/musicompass/service/ArtistReleaseServiceTest.java
@@ -28,7 +28,7 @@ class ArtistReleaseServiceTest {
         //THEN
         assertThat(actual.size(), is(2));
         assertThat(actual.get(0).getGlobalRating() > actual.get(1).getGlobalRating(), is(true));
-        assertThat(actual.get(0), equalTo(getArtistReleaseList().get(0)));
+        assertThat(actual.get(1), equalTo(getArtistReleaseList().get(0)));
     }
 
     @Test
@@ -88,7 +88,7 @@ class ArtistReleaseServiceTest {
                 .coverImageUrl("someOtherCoverImageUrl")
                 .releaseStats(stats1)
                 .build();
-        Stats stats2 = Stats.builder().numberOfWants(110).numberOfHaves(88).build();
+        Stats stats2 = Stats.builder().numberOfWants(122).numberOfHaves(78).build();
         DiscogsMasterRelease discogsMasterRelease2 = DiscogsMasterRelease.builder()
                 .year(1999)
                 .format(Arrays.asList("Album","LP"))
