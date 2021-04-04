8c34aab86301411cc73f6f636b38956f4cb6b74d
PhilippHackner <philipp.hackner@protonmail.com>
2021-04-04 14:01:39 +0200

    Release pages set up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,23 +4,27 @@ import Landing from './pages/Landing'
 import ArtistSearchResult from './pages/ArtistSearchResult'
 import ArtistOverview from './pages/ArtistOverview'
 import styled from 'styled-components/macro'
+import ArtistReleaseOverview from "./pages/ArtistReleaseOverview";
 
 export default function App() {
   return (
-    <PageLayout>
-      <AppHeader/>
-      <Switch>
-        <Route exact path="/">
-          <Landing />
-        </Route>
-        <Route exact path="/artistsearch/:artistSearchTerm">
-          <ArtistSearchResult />
-        </Route>
-          <Route exact path="/artist/:artistName">
-              <ArtistOverview/>
-          </Route>
-      </Switch>
-    </PageLayout>
+      <PageLayout>
+          <AppHeader/>
+          <Switch>
+              <Route exact path="/">
+                  <Landing/>
+              </Route>
+              <Route exact path="/artistsearch/:artistSearchTerm">
+                  <ArtistSearchResult/>
+              </Route>
+              <Route exact path="/artist/:artistName">
+                  <ArtistOverview/>
+              </Route>
+              <Route exact path="/artist/:artistName/:releaseType">
+                  <ArtistReleaseOverview/>
+              </Route>
+          </Switch>
+      </PageLayout>
   )
 }
 
---
+++ frontend/src/pages/ArtistReleaseOverview.js
@@ -0,0 +1,90 @@
+import {Link, useHistory, useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
+import {getArtistByName} from "../service/musiComApiService";
+import styled from "styled-components/macro";
+import ArtistHeader from "../components/ArtistHeader";
+import ReleaseDescription from "../components/ReleaseDescription";
+
+export default function ArtistReleaseOverview(){
+    const {artistName} = useParams();
+    const {releaseType} = useParams();
+    const [artist, setArtist] = useState();
+    const history = useHistory();
+
+    useEffect(() => {
+        getArtistByName(artistName)
+            .then(setArtist)
+            .catch((error) => console.error(error))
+    },[artistName]);
+
+    if (!artist){
+        return(
+            <section>
+                Loading
+            </section>
+        )
+    }
+
+    function releaseList () {
+        if(releaseType === "albums"){
+            return (
+                <>
+                    {artist.artistAlbums.slice(0, 5).map((release) =>
+                        <ReleaseDescription release={release}/>)}
+                </>
+            )
+        }else if (releaseType === "singles-EPs"){
+            return (
+                <>
+                    {artist.artistSingles.slice(0, 5).map((release) =>
+                        <ReleaseDescription release={release}/>)}
+                </>
+            )
+        }
+    }
+
+    return(
+        <ReleaseOverview>
+            <ArtistHeader key={artist.artistName} artist={artist}/>
+            <h3>Most popular/wanted {releaseType.replace("-", "/")}</h3>
+            {releaseList()}
+            <Link to={`artist/${artistName}`}>
+                <h5>back to artist page ...</h5>
+            </Link>
+        </ReleaseOverview>
+    )
+
+}
+
+
+
+
+/*{artist.artistAlbums.slice(0, 5).map((release) =>
+    <ReleaseDescription release={release}/>)}*/
+
+const ReleaseOverview = styled.div`
+
+  display: flex;
+  flex-direction: column;
+  background-color: var(--secondary-color);
+  padding: 20px;
+  
+  h3{
+    margin: 10px;
+    font-size: 0.9em;
+  }
+  
+  h4{
+    margin: 20px;
+  }
+  
+  h5{
+    margin: 0px 20px;
+  }
+  
+  a{
+    text-align: right;
+    margin: 10px;
+  }
+
+`
