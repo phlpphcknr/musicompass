72909648c5ab660e36a8dea0cbd3bd8c7561a5de
PhilippHackner <philipp.hackner@protonmail.com>
2021-04-07 12:15:11 +0200

    Inline bottom border added

--- frontend/src/components/AppHeader.js
+++ frontend/src/components/AppHeader.js
@@ -14,6 +14,8 @@ const Header = styled.header`
   box-shadow: 0px 2px 8px #333;
   padding: 10px 0px 20px 0px;
   font-family: 'Orange Juice';
+  border-bottom: medium solid moccasin;
+  
   h1 {
     margin: 0px;
     padding: 8px;
